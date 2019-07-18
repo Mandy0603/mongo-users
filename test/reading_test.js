18b388abac214d6fd3eda2a3e742de001360fc57
Mandy Ma <yandim0317@gmail.com>
2019-07-18 10:12:43 +1000

    Added pagination

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,21 +2,43 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users out of the database", () => {
-  let joe;
+  let joe, maria, alex, zach;
+
   beforeEach(done => {
-    joe = new User({ name: "joe" });
-    joe.save().then(() => done());
+    alex = new User({ name: "Alex" });
+    joe = new User({ name: "Joe" });
+    maria = new User({ name: "Maria" });
+    zach = new User({ name: "Zach" });
+
+    Promise.all([joe.save(), alex.save(), maria.save(), zach.save()]).then(() =>
+      done()
+    );
   });
+
   it("finds all users with a name of joe", done => {
-    User.find({ name: "joe" }).then(users => {
+    User.find({ name: "Joe" }).then(users => {
       assert(users[0]._id.toString() === joe._id.toString());
       done();
     });
   });
-  it("finds a user with a particular id", done => {
+
+  it("find a user with a particular id", done => {
     User.findOne({ _id: joe._id }).then(user => {
-      assert(user.name === "joe");
+      assert(user.name === "Joe");
       done();
     });
+  });
+
+  it("can skip and limit the result set", done => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then(users => {
+        assert(users.length === 2);
+        assert(users[0].name === "Joe");
+        assert(users[1].name === "Maria");
+        done();
+      });
   });
 });
